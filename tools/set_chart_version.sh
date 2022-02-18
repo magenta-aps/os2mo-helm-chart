b55c8a46a23ac10c55ec5a694a1dc111455e8acf
Casper V. Kristensen <vk@magenta.dk>
2022-02-18 14:49:27 +0100

    [#48629] Add support for review app

--- tools/set_chart_version.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# SPDX-FileCopyrightText: Magenta ApS
-#
-# SPDX-License-Identifier: MPL-2.0
-
-sed -i '/^version/d' chart/Chart.yaml
-grep "^version = " pyproject.toml | sed 's/ = /: /g' | tr -d '"' >> chart/Chart.yaml
